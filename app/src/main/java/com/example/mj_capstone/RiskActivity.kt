ecbd18b4e5a422bb36742211918666b41dc4d42b
김세진 <kimsejin@gimsejin-ui-MacBook-Pro.local>
2020-06-09 21:31:45 +0900

    risk layout

--- app/src/main/java/com/example/mj_capstone/RiskActivity.kt
+++ app/src/main/java/com/example/mj_capstone/RiskActivity.kt
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity
 
 class RiskActivity : AppCompatActivity(){
 
+
+
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         setContentView((R.layout.activity_risk))
